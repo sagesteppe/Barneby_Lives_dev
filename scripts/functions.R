5f94fab9c5748494544febe21b96fae36d155d67
sagesteppe <reedbenkendorf27@gmail.com>
2023-05-11 13:26:29 -0700

    map maker script and content order for labels

--- scripts/functions.R
+++ scripts/functions.R
@@ -244,3 +244,30 @@ spell_check <- function(x) {
     }
   }
 }
+
+
+#' make a quick county dot map to display the location of the collection
+#' 
+#' @param x an sf dataframe of coordinates to make maps for, requires collection number and spatial attributes
+#' @param path a directory to store the map images in before merging
+#' @param collection_col column specify the collection number or other UNIQUE id for the collection
+
+map_maker <- function(x, states, path, collection_col){
+  
+  if(st_crs(x) == sf::st_crs(states)) {
+    pts } else {pts <- sf::st_transform(x, sf::st_crs(states))}
+  
+  ints <- sf::st_intersection(pts, states)
+  
+  focal_state <- dplyr::filter(states, STATE == ints$STATE)
+  
+  p <- ggplot() + 
+    geom_sf(data = focal_state, fill = NA, color = 'grey15') + 
+    geom_sf(data = pts, size = 0.5) +
+    theme_void() 
+  
+  fname <- file.path(path, 'maps', paste0('map_', sf::st_drop_geometry(x[1,'collectionNo']), '.png'))
+  ggsave(filename =  fname, plot = p, device = 'png', dpi = 300, width = 1, height = 1, units = 'in', 
+         bg = 'transparent')
+}
+
