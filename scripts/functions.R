dea02aec44bf8ca9ce6bffedc30a9c523a2cd754
sagesteppe <reedbenkendorf27@gmail.com>
2023-05-11 17:06:28 -0700

    cleaning places

--- scripts/functions.R
+++ scripts/functions.R
@@ -315,49 +315,24 @@ place_grabber <- function(x) {
   places <- sf::st_read('../geodata/places/places.shp', quiet = T)
   
   x <- sf::st_join(x, mountains)
+
+  nearest_place <- places[sf::st_nearest_feature(dummy_pts, places),]
+  distance <- round(
+    as.numeric(
+      st_distance(x, nearest_place, by_element = T)),
+    -2) / 1000 # distance in kilometers
   
-  st_nearest(x, places)
-  x <- sf::st_join(x, places)
+  azimuth <- nngeo::st_azimuth(
+    nearest_place, 
+    x) |> round(0)
+  
+  places <- data.frame(place = st_drop_geometry(nearest_place), distance, azimuth)
+  
+  x <- dplyr::bind_cols(x, places) %>% 
+    relocate(any_of(c('place', 'distance', 'azimuth')),
+             .before = geometry)
   
   return(x)
   
 }
 
-
-
-
-#' gather distance and azimuth from shop to center of town
-#'
-#' Help provide some simple context between the building and the 
-#' @param x an sf/tibble/dataframe of locations with associated nearest locality data
-#' @param places_data an sf/dataframe/dataframe which contains coordinates for the locations in the data
-distAZE <- function(x, places_data){
-  
-  locality <- sf::st_drop_geometry(x)
-  locality <- locality[1, 'Locality']
-  
-  focal <- places_data[grep(locality, places_data$NAME), ]
-  if(nrow(focal) > 1){
-    union_loc <- sf::st_union(x) |> sf::st_point_on_surface()
-    focal <- focal[sf::st_nearest_feature(union_loc, focal),]
-  }
-  
-  location_from <- sf::st_centroid(focal)
-  
-  location_from <- sf::st_transform(location_from, 5070)
-  x_planar <- sf::st_transform(x, 5070)
-  distances <- sf::st_distance(location_from, x_planar, which = 'Euclidean')
-  
-  azy <- nngeo::st_azimuth(
-    location_from, 
-    x_planar
-  )
-  
-  distances <- data.frame(
-    st_drop_geometry(x),
-    Distance = round(as.numeric(distances / 1609.34), 1),
-    Azimuth = round(as.numeric(azy), 0),
-    geometry = x[,'geometry']
-  )
-  
-}
