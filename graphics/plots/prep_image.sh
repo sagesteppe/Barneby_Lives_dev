c89130c0393a694fc234caca1f22b341d12051fd
sagesteppe <reedbenkendorf27@gmail.com>
2025-04-28 11:55:08 -0500

    workflow image slightly more legible

--- graphics/plots/prep_image.sh
+++ graphics/plots/prep_image.sh
@@ -3,4 +3,5 @@
 mogrify -trim *;
 convert -gravity center -append Spatmod.png Taxmod.png Formatmod.png Reviewmod.png Resultmod.png caption.png workflow.png; 
 convert workflow.png -fuzz 2% -transparent white workflow.png
+find . -name '*.png~' -delete
 exit
--- manuscript/flowchart.R
+++ manuscript/flowchart.R
@@ -1,15 +1,15 @@
 library(tidyverse)
 library(DiagrammeR)
 library(ggpubr)
-setwd('/media/steppe/hdd/Barneby_Lives-dev/manuscript')
-
+#setwd('/media/steppe/hdd/Barneby_Lives-dev/manuscript')
+setwd('/home/sagesteppe/Documents/assoRted/Barneby_Lives_dev/manuscript')
 
 grViz("
 
 digraph boxes_and_circle {
-  graph[rankdir = LR, ratio = 0.1]
+  graph[rankdir = LR, ratio = 0.2, nodesep = 0.01, ranksep = 0.01]
   
-  node [shape = circle, fontsize = 36, fontname = arial, style = filled, fontcolor = White]
+  node [shape = circle, fontsize = 72, fontname = arial, style = filled, fontcolor = White]
 
       subgraph cluster_import{color= 'white'
         node [color = 'black', shape = egg]
@@ -20,24 +20,25 @@ digraph boxes_and_circle {
       subgraph cluster_geospatial {style=dashed; color= 'white'; 
         node [color = '#94524A']
   
-          'Raw\nSpreadsheet' -> 'Convert\nCoordinates\nfrom\nDMS to DD' 
-              [label = '     dms2dd', fontsize = 50, penwidth = 6, style = dashed]
-          'Convert\nCoordinates\nfrom\nDMS to DD' -> 'Check for\nautofill\nvalues' 
-              [label = '     autofill_checker', fontsize = 50, penwidth = 6, style = dashed]
+          'Raw\nSpreadsheet' -> 'Convert\nfrom\nDMS to DD' 
+              [label = '     dms2dd', fontsize = 72, penwidth = 6, style = dashed]
+          'Convert\nfrom\nDMS to DD' -> 'Check for\nautofill\nvalues' 
+              [label = '     autofill_checker', fontsize = 72, penwidth = 6, style = dashed]
           'Check for\nautofill\nvalues' -> 'Geospatial\nData' 
-              [label = '     coords2sf', fontsize = 50, penwidth = 6]
+              [label = '     coords2sf', fontsize = 72, penwidth = 6]
           'Raw\nSpreadsheet' -> 'Check for\nautofill\nvalues' 
-              [label = '     autofill_checker', fontsize = 50, penwidth = 6]
+              [label = '     autofill_checker', fontsize = 72, penwidth = 6]
           'Geospatial\nData' -> 'Political\nBoundaries' 
-              [label = '     political_grabber', fontsize = 50, penwidth = 6]
+              [label = '     political_grabber', fontsize = 72, penwidth = 6]
           'Political\nBoundaries' -> 'Site\nCharacteristics' 
-              [label = '     physical_grabber', fontsize = 50, penwidth = 6]
+              [label = '     physical_grabber', fontsize = 72, penwidth = 6]
           'Site\nCharacteristics' -> 'Site\nName' 
-              [label = '     site_writer', fontsize = 50, penwidth = 6]
+              [label = '     site_writer', fontsize = 72, penwidth = 6]
               
-          'Geospatial\nData' -> 'Directions'
-              [label = '     directions_grabber', fontsize = 50, penwidth =6, style=dashed]
-          'Directions' -> 'Site\nName'  [penwidth = 6, style=dashed]
+          'Site\nCharacteristics' -> 'Directions'
+              [label = '     directions_grabber', fontsize = 72, penwidth =6, style=dashed]
+          'Directions' -> 'Site\nName'  
+              [label = '     site_writer', fontsize = 72, penwidth =6, style=dashed]
       }
       }") %>%  
   DiagrammeRsvg::export_svg() %>%
@@ -49,9 +50,9 @@ digraph boxes_and_circle {
 grViz("
 
 digraph boxes_and_circle {
-  graph[rankdir = LR, ratio = 0.1]
+  graph[rankdir = LR, ratio = 0.1, nodesep = 0.01, ranksep = 0.01]
   
-  node [shape = circle, fontsize = 36, fontname = arial, style = filled, fontcolor = White]
+  node [shape = circle, fontsize = 72, fontname = arial, style = filled, fontcolor = White]
       
       subgraph cluster_import{color= 'white'
         node [color = '#94524A', shape = egg]
@@ -63,16 +64,16 @@ digraph boxes_and_circle {
         node [color = '#176B87']
   
           '  Spatial\nData  ' -> 'Spell\nCheck\n(Binomial)' 
-              [label = '     spell_check', fontsize = 50, penwidth = 6]
+              [label = '     spell_check', fontsize = 72, penwidth = 6]
           'Spell\nCheck\n(Binomial)' -> 'Spell\nCheck\n(Family)' 
-              [label = '     spell_check_family', fontsize = 50, penwidth = 6]
+              [label = '     spell_check_family', fontsize = 72, penwidth = 6]
           'Spell\nCheck\n(Family)' -> 'Spell\nCheck\n(Authorities)' 
-              [label = '     author_check', fontsize = 50, penwidth = 6]
+              [label = '     author_check', fontsize = 72, penwidth = 6]
           'Spell\nCheck\n(Authorities)' -> 'Spell\nCheck\n(Associated\nSpecies)' 
-              [label = '    associates_check', fontsize = 50, penwidth = 6]
+              [label = '    associates_check', fontsize = 72, penwidth = 6]
               
           'Spell\nCheck\n(Binomial)' -> 'Synonym\nCheck' 
-              [label = '    powo_searcher', fontsize = 50, penwidth = 6, style=dashed]
+              [label = '    powo_searcher', fontsize = 72, penwidth = 6, style=dashed]
            'Synonym\nCheck' -> 'Spell\nCheck\n(Associated\nSpecies)'  [penwidth = 6, style=dashed]
       }
       }") %>%  
@@ -85,10 +86,10 @@ digraph boxes_and_circle {
 grViz("
 
 digraph boxes_and_circle {
-  graph[rankdir = LR, ratio = 0.1]
+  graph[rankdir = LR, ratio = 0.1, nodesep = 0.01, ranksep = 0.01]
   
   # Formatting
-  node [shape = circle, fontsize = 36, fontname = arial, style = filled, fontcolor = White]
+  node [shape = circle, fontsize = 72, fontname = arial, style = filled, fontcolor = White]
   
     subgraph cluster_import{color= 'white'
         node [color = '#176B87', shape = egg]
@@ -100,11 +101,11 @@ digraph boxes_and_circle {
         style=dashed; color='white'; 
         node [color = '#A27E6F']
           'Taxonomic\nData' -> 'Remove\ncollection\nfrom\nAssociated\nSpecies' 
-              [label = '    associate_dropper', fontsize = 50, penwidth = 6]
+              [label = '    associate_dropper', fontsize = 72, penwidth = 6]
           'Remove\ncollection\nfrom\nAssociated\nSpecies' -> 'Check\nField\nLengths'
-              [label = '     field_lengths', fontsize = 50, penwidth = 6]
+              [label = '     field_lengths', fontsize = 72, penwidth = 6]
           'Check\nField\nLengths' -> 'Date\nFormatting' 
-              [label = '     date_parser', fontsize = 50, penwidth = 6]
+              [label = '     date_parser', fontsize = 72, penwidth = 6]
 
       }
       }
@@ -120,8 +121,8 @@ grViz("
 
 digraph boxes_and_circle {
   
-  graph[rankdir = LR, ratio = 0.1]
-  node [shape = circle, fontsize = 36, fontname = arial, style = filled, fontcolor = White]
+  graph[rankdir = LR, ratio = 0.1, nodesep = 0.01, ranksep = 0.01]
+  node [shape = circle, fontsize = 72, fontname = arial, style = filled, fontcolor = White]
   
     subgraph cluster_import{color= 'white'
         node [color = '#A27E6F', shape = egg]
@@ -133,7 +134,7 @@ digraph boxes_and_circle {
               [penwidth = 6]
                             
           'Formatted\nData' -> 'Geodata' 
-              [label = '     geodata_writer', fontsize = 50, penwidth = 6, style=dashed]
+              [label = '     geodata_writer', fontsize = 72, penwidth = 6, style=dashed]
           'Geodata' -> 'Processed\nSpreadsheet'
               [penwidth = 6, style = dashed]
               
@@ -152,9 +153,9 @@ grViz("
 
 digraph boxes_and_circle {
   
-  graph[rankdir = LR, ratio = 0.1]
+  graph[rankdir = LR, ratio = 0.1, nodesep = 0.01, ranksep = 0.01]
   
-  node [shape = circle, fontsize = 36, fontname = arial, style = filled, fontcolor = White]
+  node [shape = circle, fontsize = 72, fontname = arial, style = filled, fontcolor = White]
 
   subgraph cluster_import{color= 'white'
         node [color = '#2F0A28', shape = egg]
@@ -165,20 +166,19 @@ digraph boxes_and_circle {
         node [color = '#8DAB7F']
         
           'Clean\nData' -> 'Label\nMaker' 
-              [label = '    labels\nskeleton', fontsize = 50, penwidth = 6]
+              [label = '    labels\nskeleton', fontsize = 72, penwidth = 6]
           'Label\nMaker' -> 'Label\nAssembly' 
-              [label = '    render_labels.sh', fontsize = 50, penwidth = 6]
+              [label = '    render_labels.sh', fontsize = 72, penwidth = 6]
           'Label\nAssembly' -> 'Database\nImport' 
-              [label = '    format_database_import', fontsize = 50, penwidth = 6]
+              [label = '    format_database_import', fontsize = 72, penwidth = 6]
           'Database\nImport' -> 'Shipping\nManifest' 
-              [label = '    manifest\nskeleton', fontsize = 50, penwidth = 6] 
+              [label = '    manifest\nskeleton', fontsize = 72, penwidth = 6] 
               }  }") %>% 
   DiagrammeRsvg::export_svg() %>%
   charToRaw %>% 
   rsvg::rsvg_png("../graphics/plots/Resultmod.png")
 
 
-library(ggpubr)
 text <- paste(
   "Sample workflow for using BarnebyLives on data which has been manually entered into a spreadsheet.",
   "The top two rows indicate the main data cleaning functionality and are best run in the order outlined",
@@ -190,9 +190,9 @@ text <- paste(
   sep = "\n")
 
 # Create a text grob
-tgrob <- text_grob(text, face = "italic")
+tgrob <- text_grob(text, face = "italic", size = 18)
 # Draw the text
 as_ggplot(tgrob)
 
-ggsave('../graphics/plots/caption.png', width = 9, units = 'in')
+ggsave('../graphics/plots/caption.png', width = 14, units = 'in')
 
